057e03cab38d1725d315c40fb6833f5877c64d92
Reema Gowda <reemagowdan@gmail.com>
2020-07-28 16:50:33 +0530

    test using shallow function

--- src/App.test.js
+++ src/App.test.js
@@ -3,16 +3,33 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
 //counter function to test 
 
 describe(" counter testing", () => {
   test('render title of render', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText("This is counter app");
-    expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App />)
+    expect(wrapper.find("h1").text()).toContain("This is counter app")
+    console.log(wrapper.debug());
   });
-  
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText("This is counter app");
+  // expect(linkElement).toBeInTheDocument();
+
+
+
 
 })
+
+
+//adding shallow - shallow is the fucntion of 
+//enzyme which is actullay create instance of component.
+//shallow render the outer part of component
+
 
 
